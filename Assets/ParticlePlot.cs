8af0ca8f086fa7369379e01f99e1849a4e0e59da
David Ball <davidrball@email.arizona.edu>
2019-12-06 13:26:11 -0700

    csv reader implemented, with color

--- Assets/ParticlePlot.cs
+++ Assets/ParticlePlot.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class ParticlePlot : MonoBehaviour
 {
@@ -18,9 +19,13 @@ public class ParticlePlot : MonoBehaviour
     public string yName;
     public string zName;
 
+
+    //can change scale of plot
+    public float plotScale = 10;
+
     public GameObject PointPrefab;
 
-    public GameObject PointPrefab2;
+    public GameObject PointHolder;
 
     // Start is called before the first frame update
     void Start()
@@ -42,19 +47,101 @@ public class ParticlePlot : MonoBehaviour
     yName = columnList[columnY];
     zName = columnList[columnZ];
 
+    // Get maxes of each axis
+    float xMax = FindMaxValue(xName);
+    float yMax = FindMaxValue(yName);
+    float zMax = FindMaxValue(zName);
+ 
+    // Get minimums of each axis
+    float xMin = FindMinValue(xName);
+    float yMin = FindMinValue(yName);
+    float zMin = FindMinValue(zName);
+
+
     //Loop through Pointlist
     for (var i = 0; i < pointList.Count; i++)
     {
     // Get value in poinList at ith "row", in "column" Name
-    float x = System.Convert.ToSingle(pointList[i][xName]);
-    float y = System.Convert.ToSingle(pointList[i][yName]);
-    float z = System.Convert.ToSingle(pointList[i][zName]);
+    //loat x = System.Convert.ToSingle(pointList[i][xName]);
+    //float y = System.Convert.ToSingle(pointList[i][yName]);
+    //float z = System.Convert.ToSingle(pointList[i][zName]);
  
+
+    // Get value in poinList at ith "row", in "column" Name, normalize
+    float x = 
+    (System.Convert.ToSingle(pointList[i][xName]) - xMin) / (xMax - xMin);
+ 
+    float y = 
+    (System.Convert.ToSingle(pointList[i][yName]) - yMin) / (yMax - yMin);
+ 
+    float z = 
+    (System.Convert.ToSingle(pointList[i][zName]) - zMin) / (zMax - zMin);
+
+    
+
+
     //instantiate the prefab with coordinates defined above
-    Instantiate(PointPrefab, new Vector3(x, y, z), Quaternion.identity);
+    //Instantiate(PointPrefab, new Vector3(x, y, z), Quaternion.identity);
  
-    }
+    // Instantiate as gameobject variable so that it can be manipulated within loop
+    GameObject dataPoint = Instantiate(
+    PointPrefab, 
+    new Vector3(x, y, z)* plotScale, 
+    Quaternion.identity);
+
+    dataPoint.transform.parent = PointHolder.transform;
+
+    // Assigns original values to dataPointName
+    string dataPointName = 
+    pointList[i][xName] + " "
+    + pointList[i][yName] + " "
+    + pointList[i][zName];
+
+    // Assigns name to the prefab, prob don't want this for when we do BH particles
+    dataPoint.transform.name = dataPointName;
+
+    // Gets material color and sets it to a new RGBA color we define
+    dataPoint.GetComponent<Renderer>().material.color = 
+    new Color(x,y,z, 1.0f);
+
 
     }
+
+    
+
+    }
+ private float FindMaxValue(string columnName)
+    {
+        //set initial value to first value
+        float maxValue = Convert.ToSingle(pointList[0][columnName]);
+ 
+        //Loop through Dictionary, overwrite existing maxValue if new value is larger
+        for (var i = 0; i < pointList.Count; i++)
+        {
+            if (maxValue < Convert.ToSingle(pointList[i][columnName]))
+                maxValue = Convert.ToSingle(pointList[i][columnName]);
+        }
+ 
+        //Spit out the max value
+        return maxValue;
+    }
+ 
+    private float FindMinValue(string columnName)
+    {
+ 
+        float minValue = Convert.ToSingle(pointList[0][columnName]);
+ 
+        //Loop through Dictionary, overwrite existing minValue if new value is smaller
+        for (var i = 0; i < pointList.Count; i++)
+        {
+            if (Convert.ToSingle(pointList[i][columnName]) < minValue)
+                minValue = Convert.ToSingle(pointList[i][columnName]);
+        }
+ 
+        return minValue;
+    }
+
+
+
 
 }
